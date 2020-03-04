9b4197f83b4f674106404f93dde2057e7c4439d6
mazengyi <m.zengyi@gmail.com>
2020-03-05 00:14:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-const chokidar = require('chokidar');
-const fastuiHandler = require('./src/fastui-loader');
+const chokidar = require("chokidar");
+const fastuiHandler = require("./src/fastui-loader");
+const WebSocket = require("ws");
 
+// 启动 WebSocket Server
+const wss = new WebSocket.Server({ port: 8080 });
+wss.on("connection", function connection(ws) {
+  // 已经启动Server
+  console.log("ws connection");
+  try {
+    chokidar.watch("examples").on("all", (event, path) => {
+      var result = fastuiHandler(path);
+      console.log(JSON.stringify(result));
+      ws.send(JSON.stringify(result));
+    });
+  } catch (error) {
+    console.log(error);
+  }
 
-try {
-  chokidar.watch('./examples').on('all', (event, path) => {
-    var result = fastuiHandler(path);
-    console.log(result);
-    console.log(event, path);
-  });
-} catch (error) {
-  console.log(error);
-}
-
- // 启动 WebSocket Server
-//  const wss = new WebSocket.Server({ port: 8080});
-//  wss.on("connection", function connection(ws) {
-//    // 已经启动Server
-//    console.log("ws connection");
-//    ws.on("message", function incoming(message) {
-//      console.log("received: %s", message);
-//    });
-//    ws.send("something");
-//  });
  
-//  console.log("已经启动Sever: %s", "ws://localhost:8080");
+});
+
+ console.log("已经启动Sever: %s", "ws://localhost:8080");
